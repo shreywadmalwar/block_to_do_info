bcdcd5aa663de6d14f93ac6d57472ec2cb0072a6
Shreyash <sdwadmalwar@gmail.com>
2023-08-09 14:45:23 +0530

    pushin gfinal

--- src/edit.js
+++ src/edit.js
@@ -1,11 +1,41 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
 import './editor.scss';
 
 export default function Edit() {
+	const data = useSelect((select) => {
+		const store = select('blocks-course/todos');
+		if (!store) return null;
+		return {
+			total: store.getTodosNumber(),
+			done: store.getDoneTodos(),
+			undone: store.getUnDoneTodos(),
+		};
+	});
 	return (
-		<p {...useBlockProps()}>
-			{__('Boilerplate – hello from the editor!', 'boilerplate')}
-		</p>
+		<div {...useBlockProps()}>
+			{!data && (
+				<p>
+					{__(
+						'Please make sure the plugin is active.',
+						'todo-list-info'
+					)}
+				</p>
+			)}
+			{data && (
+				<ul>
+					<li>
+						{__('Done:', 'todo-list-info')} {data.done}
+					</li>
+					<li>
+						{__('Un-Done:', 'todo-list-info')} {data.undone}
+					</li>
+					<li>
+						{__('Total:', 'todo-list-info')} {data.total}
+					</li>
+				</ul>
+			)}
+		</div>
 	);
 }
